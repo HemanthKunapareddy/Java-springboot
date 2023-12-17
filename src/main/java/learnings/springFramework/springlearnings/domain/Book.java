ad39ba66e71cf10a5c46f8a3714d0b56336e5ecc
Hemanth Kunapareddy <kunapareddyhemanth378@gmail.com>
2023-12-17 13:26:16 +0530

    first commit

---
+++ src/main/java/learnings/springFramework/springlearnings/domain/Book.java
@@ -0,0 +1,39 @@
+package learnings.springFramework.springlearnings.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String bookName;
+    private String publisher;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
